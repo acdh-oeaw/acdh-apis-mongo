7de71baf9933192f1f459b5732437e833bc6cb71
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2019-07-03 15:12:11 +0200

    cmds to import entites or relations only added

---
+++ mongo/management/commands/entities_to_mongo.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from mongo.utils import entities_to_mongo
+
+
+class Command(BaseCommand):
+    # Show this when the user types help
+    help = "Dumps all relations into a mongo-db"
+
+    # A command must define handle()
+    def handle(self, *args, **options):
+        print('IMPORT ENTITIES')
+        entities_to_mongo()
+        return 'done'
