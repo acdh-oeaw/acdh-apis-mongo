315f20d7705ca05a1fbd55d2c0a82682c8a93068
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2019-07-03 12:32:55 +0200

    https://github.com/acdh-oeaw/apis-core/issues/118 relateions are dumped as well

--- mongo/management/commands/to_mongo.py
+++ mongo/management/commands/to_mongo.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 
-from mongo.utils import entities_to_mongo
+from mongo.utils import entities_to_mongo, relations_to_mongo
 
 
 class Command(BaseCommand):
@@ -9,5 +9,8 @@ class Command(BaseCommand):
 
     # A command must define handle()
     def handle(self, *args, **options):
+        print('IMPORT ENTITIES')
         entities_to_mongo()
+        print('IMPORT RELATIONS')
+        relations_to_mongo()
         return 'done'
--- mongo/utils.py
+++ mongo/utils.py
@@ -8,6 +8,9 @@ from django.conf import settings
 
 from webpage.utils import PROJECT_METADATA as PM
 
+rel_url = settings.APIS_BASE_URI
+ent_url = f"{settings.APIS_BASE_URI}/apis/api2/entity/"
+
 try:
     MONGO_URL = settings.MONGO["URL"]
 except AttributeError:
@@ -17,6 +20,7 @@ except AttributeError:
 client = MongoClient(MONGO_URL)
 db = client.apis
 entities = db.entities
+relations = db.relations
 
 
 def entities_to_mongo():
@@ -32,3 +36,47 @@ def entities_to_mongo():
             entities.find_one_and_replace({'id': x.id}, my_ent, upsert=True)
     print(datetime.datetime.now())
     print('done')
+
+
+def relations_to_mongo():
+    print(datetime.datetime.now())
+    for model in apps.get_app_config('apis_relations').get_models():
+        print(model)
+        for x in model.objects.all():
+            rel_obj = {}
+            try:
+                source_field = getattr(x, x._meta.get_fields()[-2].name)
+                rel_type = getattr(x, 'relation_type')
+                target_field = getattr(x, x._meta.get_fields()[-1].name)
+            except AttributeError:
+                source_field = None
+                rel_type = None
+                target_field = None
+            if source_field is not None:
+                rel_obj['global_id'] = f"{settings.APIS_BASE_URI}/relation/{x.id}"
+                rel_obj['id'] = x.id
+                rel_obj['relation'] = model.__name__.lower()
+                rel_obj['start_date'] = x.start_date
+                rel_obj['end_date'] = x.end_date
+                rel_obj['start_date_written'] = x.start_date_written
+                rel_obj['end_date_written'] = x.end_date_written
+                rel_obj['source'] = {
+                    "id": f"{source_field.id}",
+                    "url": f"{ent_url}{source_field.id}",
+                    "name": source_field.name
+                }
+                rel_obj['target'] = {
+                    "id": f"{target_field.id}",
+                    "url": f"{ent_url}{target_field.id}",
+                    "name": target_field.name
+                }
+                rel_obj['relation_type'] = {
+                    "id": rel_type.id,
+                    "label": rel_type.label,
+                    "name": rel_type.name
+                }
+                rel_obj['project'] = PM
+                relations.find_one_and_replace(
+                    {'global_id': rel_obj['global_id']}, rel_obj, upsert=True
+                )
+        print(datetime.datetime.now())
